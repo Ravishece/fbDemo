6546c120270b25de50efc4e8b0ce259d91b5343c
Ravishankar <ravishankargceb@gmail.com>
2018-11-19 18:15:40 +0000

    ajax call to create order implemented

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
    // alert('cuistom js file');
    $('.share-btn').click(function(){
+       $('#shareInFacebookBtn').show();
+       $('.custom-dropdown').show();
        var selectedItems = [];
        var selectedItemsString = '';
        $('.custom-checkbox').each(function(i){
@@ -11,6 +13,31 @@ $(document).ready(function(){
            }
        });
        //alert(selectedItemsString);
-       $('#modalBody').html('<div class="row selected-items">'+ selectedItemsString + '</div>');
+       $('#modalBody .content').html('<div class="row selected-items">'+ selectedItemsString + '</div>');
+   });
+   
+   
+   $('#shareInFacebookBtn').click(function(){
+       // /orderDetails/order_1256/4
+       var orderId = 'order' + Math.floor(Math.random() * 1000) + 1;
+       var orderQualifyVoteCount = $('#qualifyOrderCount').val() || 5;
+       var request = $.ajax({
+          url: "/orderDetails/"+ orderId + '/'+ orderQualifyVoteCount,
+          type: "GET",
+          dataType: 'text'
+        });
+        
+        request.done(function(msg) {
+          //alert(msg);
+            $('#modalBody .content').html('Order created successfully with orderId :' + orderId + '. Your order will be placed once the qualify count crosses 5 automatically');
+            $('#shareInFacebookBtn').hide();
+            $('.custom-dropdown').hide();
+        });
+        
+        request.fail(function(jqXHR, textStatus) {
+            $('#modalBody .content').html( "Order creation failed: " + textStatus );
+            $('#shareInFacebookBtn').hide();
+            $('.custom-dropdown').hide();
+        });
    })
 });
